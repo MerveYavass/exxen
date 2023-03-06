850ca0a452b28eb8aeb91e5c6b9394f6ccf2bade
Merve Yavaş <126982440+MerveYavass@users.noreply.github.com>
2023-03-06 18:09:58 +0300

    debug-update_register

--- exxen/settings.py
+++ exxen/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-+r%y82!$ks4!=(iyx_gqyfuubj#t#b=t5hty87qlxr14(707fj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -118,20 +118,33 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
-
-STATIC_URL = 'static/'
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
+if DEBUG == True:
+    STATIC_URL = 'static/'
+    STATICFILES_DIRS = [
+        BASE_DIR / 'static'
+    ]
+    MEDIA_URL = ''
+    MEDIA_ROOT = os.path.join(
+        BASE_DIR / 'media'
+    )
+else:
+    STATIC_URL = 'static/'
+    STATIC_ROOT = os.path.join(BASE_DIR / 'staticfiles')
+    MEDIA_URL = '/media/'
+    MEDIA_ROOT = os.path.join(
+        BASE_DIR / 'media'
+    )
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+# database files
 MEDIA_URL = ''
 MEDIA_ROOT = os.path.join(
     BASE_DIR / 'media'
 )
+
 
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
